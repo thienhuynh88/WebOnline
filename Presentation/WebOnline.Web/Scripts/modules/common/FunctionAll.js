58475568e21b66d4b537bd5c5068766a27c42ad1
thienhuynh88 <huynhdothien88@gmail.com>
2015-08-19 22:25:52 +0700

    thienhuynh_v1

---
+++ Presentation/WebOnline.Web/Scripts/modules/common/FunctionAll.js
@@ -0,0 +1,63 @@
+﻿
+PublicFunctionAll = (function () {
+    var valueDate = [];
+    function getDateInnit() {
+        
+        for (var i = 1; i < 32; i++) {
+            var dateObject = {};
+            dateObject.Key = i;
+            dateObject.Value = i;
+            valueDate.push(dateObject);
+        };
+        
+    }
+    
+    var valueMonth = [];
+    function getMonthInnit() {
+
+        for (var i = 1; i < 13; i++) {
+            var dateObject = {};
+            dateObject.Key = i;
+            dateObject.Value = i;
+            valueMonth.push(dateObject);
+        };
+
+    }
+
+
+    var valueYear = [];
+    function getYearInnit() {
+        var datenow = new Date();
+        var yearnow = datenow.getFullYear();
+        var yearstar = yearnow - 90;
+        
+        for (var i = yearstar; i < yearnow+1; i++) {
+            var dateObject = {};
+            dateObject.Key = i;
+            dateObject.Value = i;
+            valueYear.push(dateObject);
+        };
+
+    }
+
+
+    return {        
+        GetDateInnit:function() {
+            getDateInnit();
+            return valueDate;
+        },
+        
+
+        GetMonthInnit: function () {
+            getMonthInnit();
+            return valueMonth;
+        },
+        
+        GetYearInnit: function () {
+            getYearInnit();
+            return valueYear;
+        },
+        
+
+    };
+})();
---
+++ Presentation/WebOnline.Web/Scripts/modules/controllers/UserController.js
@@ -0,0 +1,54 @@
+﻿var UserController = (function () {
+
+    function innitViewUser() {
+        $("#sex").kendoDropDownList({
+            dataTextField: "Value",
+            dataValueField: "Key",
+            dataSource: [
+                            { Key: "Name", Value: "Nam" },
+                            { Key: "Nữ", Value: "Nữ" }
+            ]
+        });
+
+
+        $("#ngay").kendoDropDownList({
+            dataTextField: "Value",
+            dataValueField: "Key",
+            dataSource: PublicFunctionAll.GetDateInnit(),
+        });
+
+        $("#thang").kendoDropDownList({
+            dataTextField: "Value",
+            dataValueField: "Key",
+            dataSource: PublicFunctionAll.GetMonthInnit(),
+        });
+
+
+        $("#nam").kendoDropDownList({
+            dataTextField: "Value",
+            dataValueField: "Key",
+            dataSource: PublicFunctionAll.GetYearInnit(),
+        });
+        
+        $("#status").kendoDropDownList({
+            dataTextField: "Value",
+            dataValueField: "Key",
+            dataSource: [
+                            { Key: "1", Value: "Hoạt động" },
+                            { Key: "2", Value: "Tạm dừng" },
+                            { Key: "0", Value: "Huỷ tài khoản" }
+                        ]
+        });
+        
+        
+    }
+
+
+
+    return {
+        InnitViewUser: function () {
+            innitViewUser();
+        },
+
+    };
+})();
