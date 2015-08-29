e97d48ca4f37c0e0d69b60fe5c13f379fa3ef21c
thienhuynh88 <huynhdothien88@gmail.com>
2015-08-29 12:06:20 +0700

    thien_V3 123

---
+++ Presentation/WebOnline.Web/Controllers/CatalogueController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebOnline.Web.Controllers;
+
+namespace Royalty.Web.Controllers
+{
+    [Authorize]
+    public class CatalogueController : BaseController
+    {
+        //
+        // GET: /Catalogue/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ Presentation/WebOnline.Web/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebOnline.Web.Controllers;
+
+namespace Royalty.Web.Controllers
+{
+     [Authorize]
+    public class OrderController : BaseController
+    {
+         public ActionResult OrderList()
+         {
+             return View();
+         }
+
+         public ActionResult OrderNew()
+         {
+             return View();
+         }
+	}
+}
